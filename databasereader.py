4932f5dd6b42edc6a838cdb3f6e1f2ab10f5807e
Florian Diegruber <florian@diegruber.net>
2018-11-17 12:16:47 +0100

    Initial Code Structure Brainstorming

---
+++ databasereader.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+#%%
+
+
+import numpy as np
+import pandas as pd
+
+plantdb = pd.read_csv("plantdatabase")
+
+
+def select_userdata():
+    
+   return "Full user data"
+
+def select_wateringtimes():
+    
+   return "plants & respective wateringtime"
+
+#Timer:
+   
+
+def timer():
+    "execute a certain code every x timeframes depending on plant wateringtime"
+    
+
+
+
+#Ideas
+    
+#import sched, time
+#s = sched.scheduler(time.time, time.sleep)
+#def do_something(sc): 
+#    print "Doing stuff..."
+#    # do your stuff
+#    s.enter(60, 1, do_something, (sc,))
+#
+#s.enter(60, 1, do_something, (s,))
+#s.run()
+
+
